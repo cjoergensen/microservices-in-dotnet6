b83fc5b92f341dfb9a96afa88dd7cda9d29587ea
Christian Grønbæk Jørgensen <cgj@seas-nve.dk>
2022-06-27 10:05:40 +0200

    Added ´Shared.Messaging` project

---
+++ src/Final/Shared/Shared.Messaging/HostBuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Shared.Messaging;
+public static class HostBuilderExtensions
+{
+    public static IHostBuilder UseNServiceBus(this IHostBuilder hostBuilder, string endpointName)
+    {
+        hostBuilder = hostBuilder.UseNServiceBus(context =>
+        {
+            var endpointConfiguration = new EndpointConfiguration(endpointName);
+            endpointConfiguration.UseTransport<LearningTransport>();
+            return endpointConfiguration;
+        });
+
+        return hostBuilder;
+    }
+}
