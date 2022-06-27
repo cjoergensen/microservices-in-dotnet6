4f19e6c5fcaa57a8387c4a99f190b81d24d00657
Christian Grønbæk Jørgensen <cgj@seas-nve.dk>
2022-06-27 10:27:37 +0200

    Added Telemetry from Shared.Telemetry

--- src/Final/SmartMeter/Program.cs
+++ src/Final/SmartMeter/Program.cs
@@ -1,31 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using OpenTelemetry.Resources;
-using OpenTelemetry.Trace;
-
-// Define some important constants to initialize tracing with
-var serviceName = "SmartMeter";
-var serviceVersion = "1.0.0";
+using Shared.Telemetry;
 
 var hostBuilder = new HostBuilder()
-  .ConfigureServices(services =>
-  {
-      services.AddHostedService<SmartMeter.MeterReadingService>();
-      services.AddOpenTelemetryTracing(tracerProviderBuilder =>
-        {
-            tracerProviderBuilder
-            .AddConsoleExporter()
-            .AddSource(serviceName)
-            .SetResourceBuilder(
-                ResourceBuilder.CreateDefault()
-                    .AddService(serviceName: serviceName, serviceVersion: serviceVersion))
-            .AddGrpcClientInstrumentation()
-            .AddHttpClientInstrumentation()
-            .AddAspNetCoreInstrumentation();
-          //.AddSqlClientInstrumentation();
-        });
-  });
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<SmartMeter.MeterReadingService>();
+        services.AddTelemetry("SmartMeter", "1.0.0");
+    });
 
 await hostBuilder.RunConsoleAsync();
